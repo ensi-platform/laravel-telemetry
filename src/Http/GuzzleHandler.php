434a8992d70cecffd1b295daf9b503a5897a6080
Koryukov Ivan <koryukov@greensight.ru>
2022-03-31 20:08:40 +0300

    fix async requests

--- src/Http/GuzzleHandler.php
+++ src/Http/GuzzleHandler.php
@@ -3,6 +3,7 @@
 namespace Ensi\LaravelTelemetry\Http;
 
 use Ensi\LaravelTelemetry\Metrics;
+use GuzzleHttp\Promise\PromiseInterface;
 use Psr\Http\Message\RequestInterface;
 
 class GuzzleHandler
@@ -13,13 +14,25 @@ class GuzzleHandler
             return static function(RequestInterface $request, array $options) use ($handler) {
                 $start = microtime(true);
                 $response = $handler($request, $options);
-                $duration = microtime(true) - $start;
-
-                $endpoint = Metrics::normalizeHttpUri($request->getUri()->getPath());
-                Metrics::getInstance()->httpOutRequest($request->getUri()->getHost(),$endpoint, $duration);
+                if ($response instanceof PromiseInterface) {
+                    return $response->then(function ($result) use ($start, $request) {
+                        self::handleResponse($start, $request);
+                        return $result;
+                    });
+                } else {
+                    self::handleResponse($start, $request);
+                }
 
                 return $response;
             };
         };
+    }
+
+    public static function handleResponse($start, $request)
+    {
+        $duration = microtime(true) - $start;
+
+        $endpoint = Metrics::normalizeHttpUri($request->getUri()->getPath());
+        Metrics::getInstance()->httpOutRequest($request->getUri()->getHost(),$endpoint, $duration);
     }
 }
